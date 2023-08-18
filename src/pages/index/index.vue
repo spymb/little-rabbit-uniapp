41044fe53d70098cf61014fc5d5d486f8194b12d
thomas <19903766258@163.com>
2023-08-19 01:41:50 +0800

    feat: 前台分类组件

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import type { BannerItem } from '@/types/home'
+import type { BannerItem, CategoryItem } from '@/types/home'
 import CustomNavBar from './components/CustomNavBar.vue'
 import { ref } from 'vue'
-import { getHomeBannerAPI } from '@/services/home'
+import { getHomeBannerAPI, getHomeCategoryAPI } from '@/services/home'
 import { onLoad } from '@dcloudio/uni-app'
+import CategoryPanel from './components/CategoryPanel.vue'
 
 // 获取轮播图数据
 const bannerList = ref<BannerItem[]>([])
@@ -11,9 +12,16 @@ const getHomeBannerData = async () => {
   const res = await getHomeBannerAPI()
   bannerList.value = res.result
 }
+// 获取前台分类数据
+const categoryList = ref<CategoryItem[]>([])
+const getHomeCategoryData = async () => {
+  const res = await getHomeCategoryAPI()
+  categoryList.value = res.result
+}
 
 onLoad(async () => {
   await getHomeBannerData()
+  await getHomeCategoryData()
 })
 </script>
 
@@ -22,9 +30,13 @@ onLoad(async () => {
 
   <XtxSwiper :list="bannerList" />
 
+  <CategoryPanel :list="categoryList" />
+
   <view class="index"> </view>
 </template>
 
 <style lang="scss">
-//
+page {
+  background-color: #f7f7f7;
+}
 </style>
--- src/services/home.ts
+++ src/services/home.ts
@@ -1,8 +1,8 @@
-import type { BannerItem } from '@/types/home'
+import type { BannerItem, CategoryItem } from '@/types/home'
 import { http } from '@/utils/http'
 
 /**
- * 首页-广告区域-小程序
+ * 首页-广告区域
  * @param distributionSite 广告区域展示位置（投放位置 投放位置，1为首页，2为分类商品页） 默认是1
  */
 export const getHomeBannerAPI = (distributionSite = 1) => {
@@ -12,5 +12,14 @@ export const getHomeBannerAPI = (distributionSite = 1) => {
     data: {
       distributionSite,
     },
+  })
+}
+/**
+ * 首页-前台分类
+ */
+export const getHomeCategoryAPI = () => {
+  return http<CategoryItem[]>({
+    method: 'GET',
+    url: '/home/category/mutli',
   })
 }
